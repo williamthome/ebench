122b31f16c8d4548294e552d7d2e211329cac859
williamthome <willegp88@gmail.com>
2023-03-20 21:39:45 -0300

    feat(json): encode and decode bench

--- src/ebench_json.erl
+++ src/ebench_json.erl
@@ -1,22 +1,32 @@
 -module(ebench_json).
 
--export([run/0, run/1, get_json/1]).
+-export([run/1, run/2, get_json/1]).
 
-run() ->
+run(Action) ->
     % 8.1 MB JSON file
-    run(get_json("issue-90.json")).
+    run(Action, get_json("issue-90.json")).
 
-run(Filename) ->
+% The result is average iterations per second.
+% Higher is best.
+run(decode, Filename) ->
     {ok, Bin} = file:read_file(Filename),
-    % The avg is average iterations per second.
-    % Higher is best.
     ebench:run([
-        {thoas, decode, [Bin]},    % avg: 10
-        {jsx, decode, [Bin]},      % avg: 7
-        {jiffy, decode, [Bin]},    % avg: 46
-        {jsone, decode, [Bin]},    % avg: 14
-        {jsxrecord, decode, [Bin]} % avg: 7
-    ], #{samples => 20}).
+        {thoas, decode, [Bin]},    % result: 10,
+        {jsx, decode, [Bin]},      % result:  7
+        {jiffy, decode, [Bin]},    % result: 46
+        {jsone, decode, [Bin]},    % result: 14
+        {jsxrecord, decode, [Bin]} % result:  7
+    ], #{samples => 3, report => basic});
+run(encode, Filename) ->
+    {ok, Bin} = file:read_file(Filename),
+    ebench:run([
+        {thoas, encode, [ begin {ok, JSON} = thoas:decode(Bin)
+                                , JSON end ]},       % result: 10
+        {jsx, encode, [jsx:decode(Bin)]},            % result:  6
+        {jiffy, encode, [jiffy:decode(Bin)]},        % result: 30
+        {jsone, encode, [jsone:decode(Bin)]},        % result: 13
+        {jsxrecord, encode, [jsxrecord:decode(Bin)]} % result:  7
+    ], #{samples => 3, report => basic}).
 
 get_json(Filename) ->
     filename:join([ebench:priv_dir(), "json", Filename]).
